15e3738688ad970ffd3dc84f9261b03782206dcf
ccc371325cxy <ccc371325@163.com>
2020-10-18 16:34:50 +0800

    1016_practice.

---
+++ text_10_16_1/text.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+
+int main()
+{
+	double a = 0.0;
+	double b = 0.0;
+	double c = 0.0;
+	scanf("%lf%lf", &a, &b);
+	c = a + b;
+	printf("c=%.2f\n", c);
+	return 0;
+}
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
