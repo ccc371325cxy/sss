85bd7e9f922100e0721e5a6aa40af4d468f3d74c
ccc371325cxy <ccc371325@163.com>
2020-10-18 16:20:13 +0800

    1017_1.note.

---
+++ 1017_1.c
@@ -0,0 +1,29 @@
+/*
+#include<stdio.h>
+int main()
+{
+	float a,b,sum;
+	printf("任意输入两个小数，用逗号间隔:");
+	scanf("%f,%f",&a,&b);
+	sum=a+b;
+	printf("sum=%f",sum);
+	return 0;
+}
+*/
+
+//单
+
+
+/*
+#include<stdio.h>
+int main()
+{
+	double a,b,sum;
+	printf("任意输入两个小数，用逗号间隔:");
+	scanf("%lf,%lf",&a,&b);
+	sum=a+b;
+	printf("sum=%lf",sum);
+	return 0;
+	}
+	*/
+//双
